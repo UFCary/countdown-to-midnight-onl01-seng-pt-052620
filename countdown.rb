6f30c29dca04984d93f9bbe5904471ecd3c06068
Cary Williams <cary.l.williams00@gmail.com>
2020-05-29 20:45:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,8 +4,17 @@ def countdown(seconds_left)
   until seconds_left < 1 
     puts "#{seconds_left" SECONDS!"
     seconds_left -= l 
-    sleep 1 
   end
   
+  ""
+  
+end
+
+def countdown(seconds_left)
+  until seconds_left < 1 
+    puts "#{seconds_left" SECONDS!"
+    seconds_left -= l 
+    sleep 1 
+  end
   
 end

01c463b340d4f752a7760b2ef31c14ef33d700c1
Cary Williams <cary.l.williams00@gmail.com>
2020-05-29 20:49:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(seconds_left)
   until seconds_left < 1 
-    puts "#{seconds_left" SECONDS!"
+    puts "#{seconds_left} SECONDS!"
     seconds_left -= l 
   end
   "HAPPY NEW YEAR!"
@@ -11,10 +11,11 @@ end
 
 def countdown_with_sleep(seconds_left)
   until seconds_left < 1 
-    puts "#{seconds_left" SECONDS!"
+    puts "#{seconds_left} SECONDS!"
     seconds_left -= l 
     sleep 1 
   end
+  
   puts "HAPPY NEW YEAR!"
   
 end
